2e74c6585f91103e2fddd92e20d0eb4c76bcccf1
RezaeiShervin <shervinrezaei78@gmail.com>
2022-12-30 18:00:46 +0330

    Second HomeWork

---
+++ Shervin_rezaei_HW8_MaktabSharif89(2).py
@@ -0,0 +1,10 @@
+# import re
+# txt = "TheFirstProgrammingBootCamp"
+# titleWords = re.findall(r"[A-Z]{1}[a-z0-9]*", txt)
+# for word in titleWords:
+#     print(word, end=" ")
+#
+import re
+answer = "1bcdef aAF HeLLo"
+expression = re.match('', answer)
+print(expression)
